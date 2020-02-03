3afc4e81cd7bc720f3aea45fb90deeab1ff773cc
Edward Connell <ewconnell@gmail.com>
2020-02-03 14:16:11 -0800

    simplified test

--- Sources/platform/main.swift
+++ Sources/platform/main.swift
@@ -7,6 +7,21 @@ let p1 = Platform<AsyncCpuService>()
 let p2 = Platform<CudaService>()
 let p3 = Platform<Sharding<CudaService>>()
 
+//------------------------------------------------------------------------------
+// simulated api
+func add<T>(_ lhs: T, _ rhs: T) -> T
+    where T: TensorView & AdditiveArithmetic
+{
+    queue.add(lhs, rhs)
+}
+
+func addMore<T>(_ lhs: T, _ rhs: T) -> T
+    where T: TensorView & BinaryInteger
+{
+    queue.addMore(lhs, rhs)
+}
+
+//------------------------------------------------------------------------------
 // test parameters
 let runs = 10
 let iterations = 1000000 // 1 million
@@ -14,78 +29,33 @@ var count = 0
 var elapsed: [Double] = []
 
 // device is accessed via protocol in generic platform composition
-do {
-    let globalPlatform = Platform<CpuService>()
-    
-    // 0.000280
-    let viaProtocol: PlatformDevices = globalPlatform
-    
-    // 0.0250    100X slower!!
-//    DeviceContext.current.platform = globalPlatform
-//    let viaProtocol: PlatformDevices = DeviceContext.current.platform
+let globalPlatform = Platform<CpuService>()
 
-    let queue: DeviceQueue = viaProtocol.cpu(queue: 0)
-    
-    // simulated api
-    func add<T>(_ lhs: T, _ rhs: T) -> T
-        where T: TensorView & AdditiveArithmetic
-    {
-        queue.add(lhs, rhs)
+// add -> 0.000280  addMore -> 0.651
+let viaProtocol: PlatformDevices = globalPlatform
+
+// add -> 0.000280  addMore -> 0.651
+//let queue = globalPlatform.service.cpu.queues[0]
+
+// add -> 0.0250    100X slower!!
+//DeviceContext.current.platform = globalPlatform
+//let viaProtocol: PlatformDevices = DeviceContext.current.platform
+
+let queue: DeviceQueue = viaProtocol.cpu(queue: 0)
+
+// gather average runs
+elapsed = []
+count = 0
+for _ in 0..<runs {
+    let start = Date()
+    for i in 0..<iterations {
+        count += add(i, i)
+//        count += addMore(i, i)
+        //            count += queue.add(i, i)
+        //            count += globalPlatform.service.cpu.queues[0].add(i, i)
     }
-    
-    // gather average runs
-    elapsed = []
-    count = 0
-    for _ in 0..<runs {
-        let start = Date()
-        for i in 0..<iterations {
-            count += add(i, i)
-            //            count += queue.add(i, i)
-            //            count += globalPlatform.service.cpu.queues[0].add(i, i)
-        }
-        elapsed.append(Date().timeIntervalSince(start))
-    }
-    print("count: \(count)")
-    print("protocol based time: \(String(describing: elapsed.reduce(0.0,+) / Double(runs))))")
+    elapsed.append(Date().timeIntervalSince(start))
 }
-
-// device is accessed via class object in generic platform composition
-//do {
-//    let globalPlatform = Platform<CpuService>()
-//    let queue = globalPlatform.service.cpu.queues[0]
-//
-//    // simulated api
-//    func add<T>(_ lhs: T, _ rhs: T) -> T
-//        where T: TensorView & AdditiveArithmetic
-//    {
-//        queue.add(lhs, rhs)
-//    }
-//
-//    // gather average runs of 1 million iterations
-//    elapsed = []
-//    count = 0
-//    for _ in 0..<runs {
-//        let start = Date()
-//        for i in 0..<iterations {
-//            count += add(i, i)
-////            count += queue.add(i, i)
-////            count += platform.service.cpu.queues[0].add(i, i)
-//        }
-//        elapsed.append(Date().timeIntervalSince(start))
-//    }
-//    print("count: \(count)")
-//    print("class based time: \(String(describing: elapsed.reduce(0.0,+) / Double(runs))))")
-//}
-
-
-//let semaphore = DispatchSemaphore(value: 0)
-//DispatchQueue.global().async {
-//    let result = add(2, 3)
-//    print("async result: \(result)")
-//    semaphore.signal()
-//}
-//semaphore.wait()
-
-
-print("end")
+print("count: \(count)")
+print("protocol based time: \(String(describing: elapsed.reduce(0.0,+) / Double(runs))))")
 
--- Sources/platform/platform/DeviceProtocols.swift
+++ Sources/platform/platform/DeviceProtocols.swift
@@ -30,6 +30,9 @@ public protocol DeviceFunctions {
     func add<T>(_ lhs: T, _ rhs: T) -> T
         where T: TensorView & AdditiveArithmetic
     
+    func addMore<T>(_ lhs: T, _ rhs: T) -> T
+        where T: TensorView & BinaryInteger
+    
     func sub<T>(_ lhs: T, _ rhs: T) -> T
         where T: TensorView & AdditiveArithmetic
 }
@@ -39,6 +42,14 @@ extension DeviceFunctions {
         where T: TensorView & AdditiveArithmetic
     {
         lhs + rhs
+    }
+    
+    public func addMore<T>(_ lhs: T, _ rhs: T) -> T
+        where T: TensorView & BinaryInteger
+    {
+        let vl = [T](repeating: lhs, count: 10)
+        let vr = [T](repeating: rhs, count: 10)
+        return (zip(vl, vr).map { $0 + $1 }).reduce(0,+) / 10
     }
     
     public func sub<T>(_ lhs: T, _ rhs: T) -> T
